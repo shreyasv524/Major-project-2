acaa4a82caf8605ea2694cdfa1b2a84741d4653e
shreyasv524 <shreyasvaddepalli24@gmail.com>
2024-01-03 23:15:52 -0800

    some changesJ

--- cloudConfig.js
+++ cloudConfig.js
@@ -2,15 +2,15 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params:{
-        folder:"Wonderlust",
+        folder:"Wonderlust_listings",
         allowerdformats:["png","jpg","jpeg"],
     }
 });
